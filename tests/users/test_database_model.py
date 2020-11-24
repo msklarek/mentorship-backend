a7e1ec9cec861c6fe4bcb600e2ff4240853c08a5
Malgorzata Sinha <sklarekm@oregonstate.edu>
2020-11-23 17:13:36 -0800

    Update test_database_model.py
    Add unit tests

--- tests/users/test_database_model.py
+++ tests/users/test_database_model.py
@@ -16,7 +16,6 @@ from tests.test_data import test_admin_user
 
 class TestAdminUserModel(BaseTestCase):
     def test_is_first_user_admin(self):
-
         user = UserModel.query.filter_by(email=test_admin_user["email"]).first()
         self.assertTrue(user is not None)
         self.assertTrue(user.id is not None)
@@ -31,7 +30,6 @@ class TestAdminUserModel(BaseTestCase):
         )
 
     def test_second_user_cannot_be_admin(self):
-
         user = UserModel(
             name="User1",
             email="user1@email.com",
@@ -54,6 +52,44 @@ class TestAdminUserModel(BaseTestCase):
         self.assertTrue(user.terms_and_conditions_checked)
         self.assertIsInstance(user.registration_date, float)
         self.assertFalse(user.is_email_verified)
+
+    def test_delete(self):
+        user = UserModel(
+            name="User2",
+            email="user2@email.com",
+            username="user2",
+            password="user2_password",
+            terms_and_conditions_checked=True,
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        user = UserModel.query.filter_by(email="user2@email.com").first()
+        self.assertTrue(user is not None)
+        db.session.delete(user)
+        user = UserModel.query.filter_by(email="user2@email.com").first()
+        self.assertTrue(user is None)
+
+    def test_update(self):
+        user = UserModel(
+            name="User2",
+            email="user2@email.com",
+            username="user2",
+            password="user2_password",
+            terms_and_conditions_checked=True,
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        user = UserModel.query.filter_by(email="user2@email.com").first()
+        self.assertTrue(user is not None)
+        self.assertTrue(user.email == "user2@email.com")
+        user.email = "newemail@email.com"
+        db.session.add(user)
+        user = UserModel.query.filter_by(email="newemail@email.com").first()
+        self.assertTrue(user is not None)
+        user = UserModel.query.filter_by(email="user2@email.com").first()
+        self.assertTrue(user is None)
 
 
 if __name__ == "__main__":
